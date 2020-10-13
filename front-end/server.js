439f1a3bad487959238897523e96a0e04bd18090
MurmuDev <murmudev24@gmail.com>
2020-10-13 20:33:16 +0530

    improved sign-up setup

--- front-end/server.js
+++ front-end/server.js
@@ -1,3 +1,4 @@
+//loading environment variables
 if (process.env.NODE_ENV !== 'production'){
   require('dotenv').config()
 }
@@ -10,14 +11,14 @@ const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require('method-override')
 
+const users = require('./users')
 
+//initializing passport
 const initializedPassport = require('./passport-config')
 initializedPassport(passport,
   email=> users.find(user => user.email === email),
   id => users.find(user => user.id === id)
 )
-
-const users = []
 
 
 app.set('view-engine','ejs')
@@ -32,60 +33,8 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride('_method'))
 
-app.get('/',checkAuthenticated,(req,res)=>{
-  res.render('index.ejs',{name : req.user.name});
-})
 
+const routes = require('./routes/routes.js')
+app.use('/',routes);
 
-app.get('/login',checkNotAuthenticated,(req,res)=>{
-  res.render('login.ejs');
-})
-
-app.post('/login',checkNotAuthenticated,passport.authenticate('local',{
-  successRedirect: '/',
-  failureRedirect: '/login',
-  failureFlash : true
-}))
-
-app.get('/register',checkNotAuthenticated,(req,res)=>{
-  res.render('register.ejs');
-})
-
-app.post('/register',checkNotAuthenticated,async (req,res)=>{
-  try{
-    const hashedPassword = await bcrypt.hash(req.body.password,10)
-    userobj = {
-      id: Date.now().toString(),
-      name: req.body.name,
-      email:req.body.email,
-      password:hashedPassword
-    };
-
-    users.push(userobj)
-    res.redirect('/login')
-  }catch{
-    res.redirect('/register')
-  }
-  console.log(users);
-})
-
-app.delete('/logout',(req,res)=>{
-  req.logOut()
-  res.redirect('/login')
-})
-
-function checkAuthenticated(req,res,next){
-  if (req.isAuthenticated()){
-    return next()
-  }
-
-  res.redirect('/login');
-}
-
-function checkNotAuthenticated(req,res,next){
-  if(req.isAuthenticated()){
-    return res.redirect('/')
-  }
-  next()
-}
 app.listen(5000);
