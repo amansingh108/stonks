20ce4aa6d4a68207ed989d48e2d6a0345cadd4b5
Aman Singh <64307612+amansingh108@users.noreply.github.com>
2021-10-14 14:07:34 +0530

    Update route.js
    It is a good practice to use semicolon.

--- front-end/routes/routes.js
+++ front-end/routes/routes.js
@@ -3,56 +3,56 @@ const passport = require('passport');
 const bcrypt = require('bcrypt');
 
 
-//local sql
+//Local Sql
 const sql = require('../../sql/sql');
 
-//local mongodb
-const mongo = require('../../sql/mongo')
+//Local Mongodb
+const mongo = require('../../sql/mongo');
 
 
 router.get('/profile',checkAuthenticated,(req,res)=>{
   res.render('profile.ejs',{name:req.user.name,email:req.user.email})
-})
+});
 
 router.get('/browse',checkAuthenticated,(req,res)=>{
   res.render('browse.ejs',{name:req.user.name,email:req.user.email})
-})
+});
 
 router.get('/browse/:company',checkAuthenticated,(req,res)=>{
   res.render('company.ejs',{company:req.params.company,name:req.user.name,email:req.user.email})
-})
+});
 
 router.get('/',checkAuthenticated,(req,res,next)=>{
   res.redirect('/profile')
-})
+});
 
 router.get('/profile/holdings',checkAuthenticated,(req,res)=>{
   res.render('holding.ejs',{name:req.user.name,email:req.user.email})
-})
+});
 
 router.get('/profile/favs',checkAuthenticated,(req,res)=>{
   res.render('fav.ejs',{name:req.user.name,email:req.user.email})
-})
+});
 
 router.get('/profile/transactions',checkAuthenticated,(req,res)=>{
   res.render('transaction.ejs',{name:req.user.name,email:req.user.email})
-})
+});
 
 
 
 router.get('/login',checkNotAuthenticated,(req,res)=>{
   res.render('login.ejs');
-})
+});
 
 router.post('/login',checkNotAuthenticated,passport.authenticate('local',{
   successRedirect: '/profile',
   failureRedirect: '/login',
   failureFlash : true
-}))
+}));
 
 router.get('/register',checkNotAuthenticated,(req,res)=>{
   res.render('register.ejs');
-})
+});
 
 router.post('/register',checkNotAuthenticated,async (req,res)=>{
   try{
@@ -91,17 +91,17 @@ router.post('/register',checkNotAuthenticated,async (req,res)=>{
     res.redirect('/register')
     console.log(e);
   }
-})
+});
 
 router.delete('/logout',(req,res)=>{
   req.logOut()
   res.redirect('/login')
-})
+});
 
 
 router.get('/all',(req,res)=>{
   res.render('all.ejs')
-})
+});
 
 function checkAuthenticated(req,res,next){
   if (req.isAuthenticated()){
